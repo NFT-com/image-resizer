7ae78bb70801f34b7914eb731de4d002b5bbe470
t-morgan <tim@immutableholdings.com>
2022-09-07 09:19:59 -0400

    fix: process animated svgs

--- functions/resize.ts
+++ functions/resize.ts
@@ -1,5 +1,7 @@
-import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3'
+import { S3Client, GetObjectCommand, PutObjectCommand, GetObjectCommandOutput } from '@aws-sdk/client-s3'
 import { Readable } from 'stream'
+import { parse } from 'svgson'
+import { convert } from 'convert-svg-to-webp'
 import * as sharp from 'sharp'
 import * as fs from 'fs'
 
@@ -11,6 +13,28 @@ const REGION = process.env.REGION || 'us-east-1'
 const s3 = new S3Client({ region: REGION })
 
 const RESIZE_WIDTH = process.env.RESIZE_WIDTH ? parseInt(process.env.RESIZE_WIDTH) : 600
+
+const hasAnimateChild = (children) => {
+  if (!children.length) {
+    return false
+  }
+  return children.some(child => child.name === 'animate') || hasAnimateChild(children.reduce((children, child) => children.concat(child.children), []))
+}
+
+const isAnimatedSVGBuffer = async (imageBuf: Buffer) => {
+  const svg = await parse(imageBuf.toString('utf-8'))
+  const base64Prefix = 'data:image/svg+xml;base64'
+  const base64Imgs = svg.children.filter(child => child.attributes.href.indexOf(base64Prefix) !== -1)
+  for (let img of base64Imgs) {
+    const xml = await parse(Buffer.from(img.attributes.href.substring(base64Prefix.length+1), 'base64').toString('utf-8'))
+    return hasAnimateChild(xml.children)
+  }
+  return false
+}
+
+const isAnimatedSVG = async (imageType: string, imageBuf: Buffer) => {
+  return imageType === 'svg' && isAnimatedSVGBuffer(imageBuf)
+}
 
 export const resize = async (srcBucket: string, srcKey: string, dstBucket: string, dstKey: string, imageType: string) => {
   try {
@@ -30,19 +54,23 @@ export const resize = async (srcBucket: string, srcKey: string, dstBucket: strin
     return // empty object from s3
   }
 
-  let outBuf
+  let inBuf, outBuf
   try {
-    const inBuf = await new Promise<Buffer>((resolve, reject) => {
+    inBuf = await new Promise<Buffer>((resolve, reject) => {
       const stream = origimage.Body as Readable
       const chunks: Buffer[] = []
       stream.on('data', chunk => chunks.push(chunk))
       stream.once('end', () => resolve(Buffer.concat(chunks)))
       stream.once('error', reject)
     })
+
+    if (isAnimatedSVG(imageType, inBuf)) {
+      inBuf = await convert(inBuf)
+    }
     outBuf = await sharp(inBuf, {
       animated: imageType === 'gif' ? true : undefined,
       limitInputPixels: false
-    }).webp().resize(RESIZE_WIDTH).toBuffer()
+    }).timeout({ seconds: 14 }).webp().resize(RESIZE_WIDTH).toBuffer()
   } catch (error) {
     console.log(error, srcKey)
     return
@@ -57,6 +85,7 @@ export const resize = async (srcBucket: string, srcKey: string, dstBucket: strin
     }
 
     const putResult = await s3.send(new PutObjectCommand(destparams))
+    console.log(putResult)
 
   } catch (error) {
     console.log(error, srcKey)
